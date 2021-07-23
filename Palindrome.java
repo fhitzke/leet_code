e3d7446ad787619d0add0bf58eba2585710d8934
obstfliege24 <87448209+obstfliege24@users.noreply.github.com>
2021-07-23 09:52:18 +0200

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,7 +1,7 @@
 class Palindrome {
 
     public boolean isPalindrome(int x) {
-        if (x < 0 || (x % 10 == 0)) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
         int n = 0;
