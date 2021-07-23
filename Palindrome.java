901bbcacfa1022372135f356dd0c06746924c997
obstfliege24 <87448209+obstfliege24@users.noreply.github.com>
2021-07-23 09:30:37 +0200

    Add files via upload
    This code checks if an integer is a palindrome or not.

---
+++ Palindrome.java
@@ -0,0 +1,14 @@
+class Palindrome {
+
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0)) {
+            return false;
+        }
+        int n = 0;
+        while (x > n) {
+            n = n * 10 + x % 10;
+            x /= 10;
+        }
+        return x == n || x == n/10;
+    }
+}
