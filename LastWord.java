d353fdc4e9cf89ae38aa8bfcff8639653f8bd450
obstfliege24 <87448209+obstfliege24@users.noreply.github.com>
2021-07-26 11:01:56 +0200

    Add files via upload
    Given a string s, consisting of words separated by spaces, this code returns the length of the last word in the string.

---
+++ LastWord.java
@@ -0,0 +1,15 @@
+public class LastWord {
+
+    public static int lengthOfLastWord(String s) {
+        if (s.equals(" ") || s == null) {
+            return 0;
+        }
+        String[] words = s.split(" ");
+
+        if (words.length == 0) {
+            return 0;
+        }
+        String w = words[words.length-1];
+        return w.length();
+    }
+}
